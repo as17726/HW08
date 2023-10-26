72ce5822b07e059cda01e5bdc538541011a4df78
Ayushi Shah <as17726@nyu.edu>
2023-10-26 15:27:41 -0400

    Basic code outline

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,46 @@
+let mImage; 
+let patternImage; 
+let spacing = 32;
+
+function preload() {
+  mImage = loadImage("Mondrian.jpg"); 
+  patternImage = loadImage("pattern.jpg"); 
+}
+
+//function mosaic(X,Y) {
+//}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  pixelDensity(1); 
+  mImage.resize(0, height); 
+  noLoop(); 
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+  background(255); 
+
+  mImage.loadPixels(); 
+
+  for (let y = 0; y < mImage.height; y += spacing) {
+    for (let x = 0; x < mImage.width; x += spacing) {
+      let pixelIndex = 4 * (y * mImage.width + x);
+      let redVal = mImage.pixels[pixelIndex + 0];
+      let greenVal = mImage.pixels[pixelIndex + 1];
+      let blueVal = mImage.pixels[pixelIndex + 2];
+
+    let maxVal = max(redVal, greenVal, blueVal); 
+
+    if (maxVal == redVal && greenVal == 0 && blueVal == 0) {
+     
+    } else if (maxVal == greenVal && maxVal == redVal && blueVal == 0) {
+      
+    } else if(maxVal == blueVal && greenVal == 0 && redVal == 0){
+ 
+    }
+  }
+
+  mImage.updatePixels(); 
+  image(mImage, 0, 0); 
+  }
 }
