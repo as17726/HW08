b96e0291f9d438ff94492dfa4944dc039d9ada45
Ayushi Shah <as17726@nyu.edu>
2023-10-26 20:45:56 -0400

    song

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,11 +1,43 @@
+let song; 
+let peaks; 
+
+function preload() {
+song = loadSound("./christmas.mp3"); 
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  peaks = song.getPeaks();
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+  background(0,205,255);
+  print(peaks); 
+  noFill();
+  stroke(0);
+
+  let numShapes = peaks.length;
+  let shapeWidth = width / numShapes;
+
+  beginShape();
+  for (let i = 0; i < numShapes; i++) {
+    let shapeHeight = map(peaks[i], 0, 1, 0, height);
+    let x = i * shapeWidth;
+    let y = height - shapeHeight;
+
+    // Create vertex points for the shape
+    vertex(x, y);
+  }
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
 }
+
+function mouseClicked() {
+  if (!song.isPlaying()) {
+    song.play();
+  } else if (song.isPlaying) {
+    song.pause(); 
+  }
+}
+
